71fc49e2e4dff92ee82b7f9e29ec30e9205d514d
Tom Victor <vjtomvictor@icloud.com>
2019-01-12 21:34:22 +0530

    update apifix

--- api/serializers.py
+++ api/serializers.py
@@ -3,8 +3,8 @@ from core.models import *
 
 
 class PostSerializer(serializers.ModelSerializer):
-    timestamp = serializers.CharField(source='get_timestamp')
-    last_updated = serializers.CharField(source='get_updatestamp')
+    timestamp = serializers.CharField(source='get_timestamp', required=False)
+    last_updated = serializers.CharField(source='get_updatestamp', required=False)
     class Meta:
         model = Posts
         fields = (
@@ -15,3 +15,6 @@ class PostSerializer(serializers.ModelSerializer):
             'last_updated'
         )
         read_only_fields = ['id','timestamp','last_updated']
+
+
+
